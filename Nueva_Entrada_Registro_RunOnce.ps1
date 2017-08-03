838774ada558039016d50db33af49c7d99b127c1
joseska <joseska@gmail.com>
2017-08-03 11:24:16 +0100

    Crear Entrada Registro RunOnce
    Crear una Entrada en el Registro de Windows para ejecutar un bat una
    sola vez. RunOnce

---
+++ Nueva_Entrada_Registro_RunOnce.ps1
@@ -0,0 +1,4 @@
+﻿# Crear entrada en el registro RunOnce
+Write-Host "Creamos entrada registro RunOnce en $($computer)" -foregroundcolor "magenta"
+$RunOnceKey = "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce"
+set-itemproperty $RunOnceKey "BorraloTODO" ("c:\BorraloTodo.bat")
